da2b9186262405f192b1a71560c9e414703cfb4d
Rods <rodolfo.perez01@iest.edu.mx>
2023-08-06 17:23:52 -0500

    Final mobile navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "./styles/globals.css";
 import { Poppins, Montserrat } from "next/font/google";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";

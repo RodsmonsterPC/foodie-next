5cfbf76b4a1fa64c47815e221671abc1ba093535
Rods <rodolfo.perez01@iest.edu.mx>
2023-08-14 13:35:29 -0500

    Detail desktop and mobile

--- app/components/detailButton.jsx
+++ app/components/detailButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const detailButton = ({ name }) => {
   return (
     <div>
-      <button className="text-white text-lg bg-button-color w-description-h h-12 rounded-full mt-7 ">
+      <button className="text-white text-lg bg-button-color w-description-h h-12 rounded-full mt-7 drop-shadow-xl">
         {name}
       </button>
     </div>
--- app/pages/detailProduct.jsx
+++ app/pages/detailProduct.jsx
@@ -6,47 +6,66 @@ const detailProduct = () => {
   return (
     <div>
       <div>
-        <p className="text-3xl mt-12 ml-8">Plátano Chiapas</p>
+        <p className="text-3xl mt-12 ml-8 md:flex md:justify-center md:text-5xl md:mt-24">
+          Plátano Chiapas
+        </p>
       </div>
-      <div className="flex justify-center mr-2 mt-11">
-        <Image className="" src={"/icon-person.svg"} width={144} height={150} />
-        <div className="ml-4 mt-4 text-xl">
-          <h2>$56.00/kg</h2>
-          <div className="flex flex-col mt-4 text-xs font-bold">
-            <small>Cantidad Disponible:</small>
-            <small>20</small>
+      <div className="md:flex md:flex-row md:mb-28">
+        <div className="flex justify-center mr-2 mt-11 md:flex-col md:ml-2 md:mt-0 ">
+          <Image
+            className="md:hidden"
+            src={"/icon-person.svg"}
+            width={144}
+            height={150}
+          />
+          <Image
+            className="hidden md:flex "
+            src={"/icon-person.svg"}
+            width={414}
+            height={431}
+          />
+          <div className="ml-4 mt-4 text-xl">
+            <h2 className="md:hidden">$56.00/kg</h2>
+            <div className="flex flex-col mt-4 text-xs font-bold md:items-center  md:text-base">
+              <small>Cantidad Disponible:</small>
+              <small>20</small>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="ml-6">
-        <p className="font-bold text-sm">Descripción</p>
-        <ul className="mt-5">
-          <li className="flex mt-4">
-            <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
-            <p className="text-base">Plátano Chiapas por Kg</p>{" "}
-          </li>
-          <li className="flex mt-4">
-            <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
-            <p className="text-base">Elige una vida más saludable</p>
-          </li>
-          <li className="flex mt-4">
-            <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
-            <p className="text-base">
-              Aprovecha las promociones en este producto
-            </p>
-          </li>
-          <li className="flex mt-4">
-            <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
-            <p className="text-base">Muy delicioso sabor</p>
-          </li>
-        </ul>
-      </div>
+        <div className="ml-6 md:flex md:flex-col md:mt-16 ">
+          <p className="font-bold text-sm">Descripción</p>
+          <ul className="mt-5">
+            <li className="flex mt-4">
+              <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
+              <p className="text-base">Plátano Chiapas por Kg</p>{" "}
+            </li>
+            <li className="flex mt-4">
+              <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
+              <p className="text-base">Elige una vida más saludable</p>
+            </li>
+            <li className="flex mt-4">
+              <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
+              <p className="text-base">
+                Aprovecha las promociones en este producto
+              </p>
+            </li>
+            <li className="flex mt-4">
+              <Image src={"/check-icon.svg"} width={18} height={18} />{" "}
+              <p className="text-base">Muy delicioso sabor</p>
+            </li>
+          </ul>
+        </div>
+        <div className="md:flex md:flex-col md:ml-24">
+          <h2 className="hidden md:flex md:justify-center md:text-4xl">
+            $56.00/kg
+          </h2>
+          <Counter />
 
-      <Counter />
-
-      <div className="ml-6 flex flex-col mb-12">
-        <DetailButton name={"Compra rápida"} />
-        <DetailButton name={"Agregar el carrito"} />
+          <div className="ml-6 flex flex-col mb-12 md:items-center">
+            <DetailButton name={"Compra rápida"} />
+            <DetailButton name={"Agregar el carrito"} />
+          </div>
+        </div>
       </div>
     </div>
   );

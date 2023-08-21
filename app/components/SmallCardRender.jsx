369eebdc4032ddbe2ebf462632a4316aef603712
Mario Martinez <mariomartinezmesm@gmail.com>
2023-08-21 11:56:45 -0600

    fix:corrections on the main page,login and implementation ofimage carousel in landing

--- app/components/SmallCardRender.jsx
+++ app/components/SmallCardRender.jsx
@@ -1,67 +1,83 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import SmallCardProduct from './SmallCardProduct'
-import CarrouselButtons from './carrouselButtons'
+"use client";
+import "swiper/css";
+import "swiper/css/navigation";
+import 'swiper/css/pagination';
+import SmallCardProduct from "./SmallCardProduct";
+import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useEffect, useState} from "react";
+import SlideNavButtons from "./paginationCarrucel";
 
-const initialState = {
-   paginationInitial: 0,
-   paginationFinal: 4
-}
+const SmallCardRender = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products?limit=24"
+        );
+        const dataApi = await response.json();
+        setData(dataApi);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    })();
+  }, []);
+  return (
+    <>
+      <Swiper
+        breakpoints={{
+          0: {
+            slidesPerView:1,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+        }}
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <SmallCardProduct
+              key={item.id}
+              title={item.title}
+              price={item.price}
+              description={item.description}
+              img={item.image}
+            />
+          </SwiperSlide>
+        ))}
+        <SlideNavButtons/>
+      </Swiper>
+      <Swiper
+        breakpoints={{
+          0: {
+            slidesPerView:1,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+          },
+        }}
+        className="mt-10"
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <SmallCardProduct
+              key={item.id}
+              title={item.title}
+              price={item.price}
+              description={item.description}
+              img={item.image}
+            />
+          </SwiperSlide>
+        ))}
+        <SlideNavButtons/>
+      </Swiper>
 
-const SmallCardRender = ({ title }) => {
+    </>
+  );
+};
 
-   const [data, setData] = useState([])
-   const [carrousel, setCarrousel] = useState(initialState)
-   useEffect(() => {
-      (async () => {
-         try {
-            const response = await fetch('https://fakestoreapi.com/products?limit=24');
-            const data = await response.json();
-            setData(data)
-            console.log(data); // Hacer algo con los datos obtenidos
-         } catch (error) {
-            console.error('Fetch error:', error);
-         }
-      })()
-   }, [])
-
-   //const carrouselAction = (suma) => {
-   //   setCarrousel( {
-   //      ...carrousel,
-   //      paginationInitial: suma,
-   //      paginationFinal: suma
-   //   }
-   //)
-   //
-   //   console.log(carrousel)
-   //   console.log(carrousel.paginationInitial)
-   //}
-
-   return (
-      <>
-         <h2 className='font-bold m-3 text-lg'>{title}</h2>
-         <div className='md:flex gap-6'>
-            {
-               data.map((item, idx) => {
-                  return ((idx >= carrousel.paginationInitial && idx < carrousel.paginationFinal) &&
-                     <SmallCardProduct
-                        key={item.id}
-                        title={item.title}
-                        price={item.price}
-                        description={item.description}
-                        img={item.image}
-                     />
-                  )
-               })
-            }
-         </div>
-         <CarrouselButtons
-            carrousel={carrousel}
-            onClick={setCarrousel}
-            pagination={carrousel.paginationFinal}
-            final={data.length} />
-      </>
-   )
-}
-
-export default SmallCardRender
+export default SmallCardRender;
